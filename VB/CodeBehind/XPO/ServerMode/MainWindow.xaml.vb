3c21398708f1ca85151b6fcd9e5e0046bd2bc949
dxgoncharov <36544836+dxgoncharov@users.noreply.github.com>
2022-05-05 17:12:36 +0400

    Update MainWindow.xaml.vb

--- VB/CodeBehind/XPO/ServerMode/MainWindow.xaml.vb
+++ VB/CodeBehind/XPO/ServerMode/MainWindow.xaml.vb
@@ -7,12 +7,12 @@ Class MainWindow
     Public Sub New()
         InitializeComponent()
         Dim properties = New ServerViewProperty() {
-        New ServerViewProperty("Subject", SortDirection.None, New OperandProperty("Subject")),
-        New ServerViewProperty("UserId", SortDirection.None, New OperandProperty("UserId")),
-        New ServerViewProperty("Created", SortDirection.None, New OperandProperty("Created")),
-        New ServerViewProperty("Votes", SortDirection.None, New OperandProperty("Votes")),
-        New ServerViewProperty("Priority", SortDirection.None, New OperandProperty("Priority")),
-        New ServerViewProperty("Oid", SortDirection.Ascending, New OperandProperty("Oid"))
+            New ServerViewProperty("Subject", SortDirection.None, New OperandProperty("Subject")),
+            New ServerViewProperty("UserId", SortDirection.None, New OperandProperty("UserId")),
+            New ServerViewProperty("Created", SortDirection.None, New OperandProperty("Created")),
+            New ServerViewProperty("Votes", SortDirection.None, New OperandProperty("Votes")),
+            New ServerViewProperty("Priority", SortDirection.None, New OperandProperty("Priority")),
+            New ServerViewProperty("Oid", SortDirection.Ascending, New OperandProperty("Oid"))
         }
         Dim session = New Session()
         Dim source = New XPServerModeView(session, GetType(Issue), Nothing)
